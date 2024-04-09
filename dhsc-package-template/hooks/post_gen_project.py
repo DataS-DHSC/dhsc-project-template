b9b24489bdb3d4d6e07de7103b2e9243eb2318e1
george-w-jenkins <george.jenkins.1995@gmail.com>
2024-04-09 16:05:54 +0100

    modify path in post-gen-hook

--- dhsc-package-template/hooks/post_gen_project.py
+++ dhsc-package-template/hooks/post_gen_project.py
@@ -3,8 +3,8 @@ import shutil
 
 from pathlib import Path
 
-base_dir = Path(__file__).parents[0]
-
+ext_dir = Path(__file__).parents[1]
+base_dir = ext_dir / '{{cookiecutter.repo_name}}'
 def remove(path):
     if os.path.isfile(path):
         os.remove(path)
@@ -17,6 +17,7 @@ if not keep_example_code:
     print('remove firing')
     #remove exitsing src folder
     src_path = base_dir / 'src'
+    print(src_path)
     remove(src_path)
     #make new empty src folder
     os.makedirs(src_path, exist_ok=True)
