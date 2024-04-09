28d24567a30a8f7e3d16db54de7ae77b16f48547
george-w-jenkins <george.jenkins.1995@gmail.com>
2024-04-09 15:02:10 +0100

    modifiy environnment.yml

--- dhsc-project-template/{{ cookiecutter.repo_name }}/src/example_module/visualise.py
+++ dhsc-project-template/{{ cookiecutter.repo_name }}/src/example_module/visualise.py
@@ -1,27 +1,33 @@
 # general
 from pathlib import Path
 import logging
-# project specfic
+import ast
+
+# project specific
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import pandas as pd
+
 # custom
-from src.example_module import util
+from src.example_module import utils
 
 
-def make_plots(summary_df, nims_monthly_totals, config):
-    """runs the plotting subpipline"""
+def make_plots(
+    base_dir: Path,
+    summary_df: pd.DataFrame,
+    nims_monthly_totals: pd.DataFrame,
+    config: dict,
+):
+    """runs the plotting subpipeline"""
 
-    BASE_DIR = Path(__file__).parents[2]
-    OUTPUT_DIR = BASE_DIR / "output"
+    output_dir = base_dir / "output"
 
-    plot_nims_monthly(nims_monthly_totals, config, OUTPUT_DIR)
+    plot_nims_monthly(nims_monthly_totals, config, output_dir)
 
-    plot_summary(summary_df, config, OUTPUT_DIR)
+    plot_summary(summary_df, config, output_dir)
 
 
-# %%
 def plot_nims_monthly(
     nims_monthly_totals: pd.DataFrame, config: dict, output_path: Path
 ):
@@ -32,11 +38,14 @@ def plot_nims_monthly(
         config (dict): configuration file
         output_path (Path): dir to save plot in -
     """
-    logging.info('Plotting nims monthly data')
+    logging.info("Plotting nims monthly data")
+
+    # as config stores strings, need to use ast package to evaluate as a list
+    year_range = ast.literal_eval(config["plotting"]["nims_year_range"])
 
     fig, ax = plt.subplots()
     nims_plot = sns.lineplot(
-        data=nims_monthly_totals.query("year in [2021, 2022]"),
+        data=nims_monthly_totals.query("year in @year_range"),
         x="month",
         hue="year",
         y="total",
@@ -45,18 +54,17 @@ def plot_nims_monthly(
         linewidth=2,
     )
 
-    y_format = ticker.FuncFormatter(util.million_format)
+    y_format = ticker.FuncFormatter(utils.million_format)
     nims_plot.yaxis.set_major_formatter(y_format)
-    ax = util.set_labels(ax, "Number of NIMS GP attendances", fontsize=12)
 
-    ax = util.style_plot(ax)
+    ax = utils.set_labels(ax, "Number of NIMS GP attendances", fontsize=12)
+    ax = utils.style_plot(ax)
 
     plt.savefig(
-        output_path / f"{config['nims']['file_name']}_{config['date_stamp']}.svg"
+        output_path / f"{config['timestamp']}_{config['nims']['file_name']}.svg"
     )
 
 
-# %%
 def plot_summary(summary_df: pd.DataFrame, config: dict, output_path: Path):
     """Creates plot of proportion of GP visits by type
 
@@ -65,11 +73,14 @@ def plot_summary(summary_df: pd.DataFrame, config: dict, output_path: Path):
         config (dict): configuration file
         output_path (Path): dir to save plot in
     """
-    logging.info('Plotting attendence data')
+    logging.info("Plotting attendence data")
 
     for col in ["attended", "did_not_attend", "unknown"]:
-        util.convert_to_proportion(summary_df, col, "total_count_of_appointments")
+        summary_df.loc[:, col] = summary_df[col].div(
+            summary_df.total_count_of_appointments
+        )
 
+    # unpivot df from wide to long froma to work nicely with sns.lineplot
     melt_df = summary_df.drop(["total_count_of_appointments"], axis=1).melt(
         id_vars=["weekday", "appointment_date"],
         var_name="type",
@@ -82,19 +93,19 @@ def plot_summary(summary_df: pd.DataFrame, config: dict, output_path: Path):
         x="appointment_date",
         hue="type",
         y="proportion",
-        #  palette = 'coolwarm',
+        # palette = 'coolwarm',
         ax=ax,
         linewidth=2,
     )
     plt.xticks(melt_df.appointment_date)
 
-    x_format = ticker.FuncFormatter(util.weekday_date_formatter)
+    x_format = ticker.FuncFormatter(utils.weekday_date_formatter)
     summary_plot.xaxis.set_major_formatter(x_format)
     y_format = ticker.PercentFormatter(xmax=1)
     summary_plot.yaxis.set_major_formatter(y_format)
 
-    util.set_labels(ax, title="Proportion of GP attendances by type", fontsize=12)
-    util.style_plot(ax)
+    utils.set_labels(ax, title="Proportion of GP attendances by type", fontsize=12)
+    utils.style_plot(ax)
     plt.savefig(
-        output_path / f"{config['summary']['file_name']}_{config['date_stamp']}.svg"
+        output_path / f"{config['timestamp']}_{config['summary']['file_name']}.svg"
     )
