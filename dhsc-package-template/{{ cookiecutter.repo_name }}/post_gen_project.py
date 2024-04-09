dabd167d310938070a71eaf8cff9a6e958e9fab5
george-w-jenkins <george.jenkins.1995@gmail.com>
2024-04-09 15:48:06 +0100

    exp to make example code optional

---
+++ dhsc-package-template/{{ cookiecutter.repo_name }}/post_gen_project.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+
+from pathlib import Path
+
+base_dir = Path(__file__).parents[0]
+
+
+def remove(path):
+    if os.path.isfile(path):
+        os.remove(path)
+    elif os.path.isdir(path):
+        shutil.rmtree(path)
+
+keep_example_code = '{{cookiecutter.include_example_code}}' == 'yes'
+
+if not keep_example_code:
+    #remove exitsing src folder
+    src_path = base_dir / 'src'
+    remove(src_path)
+    #make new empty src folder
+    os.makedirs(src_path, exist_ok=True)
+
+
+
