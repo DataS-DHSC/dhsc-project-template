62c6fc0953bde5b3d1d593fa28ef50e031a311ad
george-w-jenkins <george.jenkins.1995@gmail.com>
2024-04-10 09:55:58 +0100

    make example code optional in project

---
+++ dhsc-project-template/hooks/post_gen_project.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+
+from pathlib import Path
+
+base_dir = Path(os.getcwd())
+def remove(path):
+    if os.path.isfile(path):
+        os.remove(path)
+    elif os.path.isdir(path):
+        shutil.rmtree(path)
+
+keep_example_code = '{{cookiecutter.include_example_code}}' == 'yes'
+if not keep_example_code:
+    #remove exitsing src folder
+    src_path = base_dir / 'src'
+    remove(src_path)
+    #make new empty src folder
+    os.makedirs(src_path, exist_ok=True)
+
+    
+
+
+
